3c0a12e396dec782770124448c2ab0e8f3e130a2
brettadams1 <brettadams314@gmail.com>
2023-04-18 14:57:36 +0000

    revert

--- gitpush.sh
+++ gitpush.sh
@@ -2,4 +2,5 @@
 
 git add .
 read -p 'Enter the commit message you want to use: ' commit_message
-git commit -m "$commit_message" & git push & echo "=====================" & echo "Push Successful"
+git commit -m "$commit_message"
+git push

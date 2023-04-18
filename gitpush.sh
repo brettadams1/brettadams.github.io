0620c09514e1dc70ffb292186144c4448525ad47
brettadams1 <brettadams314@gmail.com>
2023-04-18 14:55:31 +0000

    test commit

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git add .
-echo "Enter the commit message you want to use: "
-read commit_message
-git commit -m $commit_message
+read -p 'Enter the commit message you want to use: ' commit_message
+echo $commit_message
+git commit -m "$commit_message"
 git push

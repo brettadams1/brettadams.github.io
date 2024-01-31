903de46b11166eaab5a86a031238364dd2015fb1
brettadams1 <brettadams314@gmail.com>
2024-01-31 18:19:59 +0000

    stuff

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,54 +1,176 @@
 /* global $, sessionStorage */
 
-$(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
-  ////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////// SETUP /////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
+$(document).ready(runProgram); // Wait for all HTML/CSS elements to be loaded before running program
 
-  // Constant Variables
-  var FRAME_RATE = 60;
-  var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
-  // Game Item Objects
+function runProgram() {
+  // CONSTANTS AND VARIABLES DECLARATION
 
+  // Frame settings
+  const FRAME_RATE = 60;
+  const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
 
-  // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  // Key codes for arrow keys
+  const KEY = {
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+    W: 87,
+    A: 65,
+    S: 83,
+    D: 68
+  };
 
+  // Walker object containing initial values and speeds
+  let walker = {
+    xCoord: 0,
+    yCoord: 0,
+    xSpeed: 0,
+    ySpeed: 0
+  };
+
+  let PLAYER2 = {
+    xCoord: 390,
+    yCoord: 390,
+    xSpeed: 0,
+    ySpeed: 0
+  };
+  // Set up functions execution intervals
+  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);
+
+  // Event listeners registration
+  $(document).on('keydown', handleKeyDown);
+  $(document).on('keyup', handleKeyUp);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
   /* 
-  On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
-  by calling this function and executing the code inside.
+  Function called on each timer tick. Performs game logic.
   */
   function newFrame() {
-    
-
+    repositionGameItem()
+    redrawGameItem()
+    wallCollision()
+    randomColor()
   }
-  
-  /* 
-  Called in response to events.
-  */
-  function handleEvent(event) {
 
+  /* 
+  Functions called when specific keyboard events occur.
+  */
+  function handleKeyDown(event) {
+    switch (event.which) {
+      case KEY.LEFT:
+        walker.xSpeed = -5;
+        break;
+      case KEY.UP:
+        walker.ySpeed = -5;
+        break;
+      case KEY.RIGHT:
+        walker.xSpeed = 5;
+        break;
+      case KEY.DOWN:
+        walker.ySpeed = 5;
+        break;
+      case KEY.A:
+        PLAYER2.xSpeed = -5;
+        break;
+      case KEY.W:
+        PLAYER2.ySpeed = -5;
+        break;
+      case KEY.D:
+        PLAYER2.xSpeed = 5;
+        break;
+      case KEY.S:
+        PLAYER2.ySpeed = 5;
+        break;
+      default:
+        break;
+    }
+  }
+
+  function handleKeyUp(event) {
+    switch (event.which) {
+      case KEY.LEFT:
+        walker.xSpeed = 0;
+        break;
+      case KEY.UP:
+        walker.ySpeed = 0;
+        break;
+      case KEY.RIGHT:
+        walker.xSpeed = 0;
+        break;
+      case KEY.DOWN:
+        walker.ySpeed = 0;
+        break;
+      case KEY.A:
+        PLAYER2.xSpeed = 0;
+        break;
+      case KEY.W:
+        PLAYER2.ySpeed = 0;
+        break;
+      case KEY.D:
+        PLAYER2.xSpeed = 0;
+        break;
+      case KEY.S:
+        PLAYER2.ySpeed = 0;
+        break;
+      default:
+        break;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+  function randomColor() {
+    let randomColor = "#000000".replace(/0/g, () => {
+      return (~~(Math.random() * 16)).toString(16);
+    });
+
+    $("#walker").on("click", () => {
+      $("#walker").css("background-color", randomColor)
+    })
+    $("#player2").on("click", () => {
+      $("#player2").css("background-color", randomColor)
+    })
+  }
+
+  function repositionGameItem() {
+    walker.xCoord += walker.xSpeed
+    walker.yCoord += walker.ySpeed
+    PLAYER2.xCoord += PLAYER2.xSpeed
+    PLAYER2.yCoord += PLAYER2.ySpeed
+  }
+
+  function redrawGameItem() {
+    $("#walker").css("left", walker.xCoord);
+    $("#walker").css("top", walker.yCoord);
+    $("#player2").css("left", PLAYER2.xCoord);
+    $("#player2").css("top", PLAYER2.yCoord);
+  }
+
+  function wallCollision() {
+    if (walker.xCoord > $("#board").width() - $("#walker").width() || walker.xCoord < 0) {
+      walker.xCoord -= walker.xSpeed;
+    } 
+    if (walker.yCoord > $("#board").height() - $("#walker").height() || walker.yCoord <= 0) {
+      walker.yCoord -= walker.ySpeed;
+    }
+    if (PLAYER2.xCoord > $("#board").width() - $("#player2").width() || PLAYER2.xCoord < 0) {
+      PLAYER2.xCoord -= PLAYER2.xSpeed;
+    } 
+    if (PLAYER2.yCoord > $("#board").height() - $("#player2").height() || PLAYER2.yCoord <= 0) {
+      PLAYER2.yCoord -= PLAYER2.ySpeed;
+    }
+  }
+
   function endGame() {
-    // stop the interval timer
+    // Stop the interval timer
     clearInterval(interval);
 
-    // turn off event handlers
+    // Turn off event handlers
     $(document).off();
   }
-  
 }
